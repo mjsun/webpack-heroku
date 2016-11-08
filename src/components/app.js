10dd43ea057189ebf6359cf93c4a80e1eb2a06ad
mjsun <msun@mingjies-MBP.fios-router.home>
2016-11-07 20:00:54 -0500

    commit with route

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 export default class App extends React.Component {
   render () {
     return (
       <div>
-        <h1>Change me</h1>
+          <Link to="/home">Home</Link>
+          {this.props.children}
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
 import './styles/app.scss'
+import {Router, hashHistory} from 'react-router';
+import routes from './routes';
 
-render(<App/>, document.getElementById('main'))
+render(<Router history={hashHistory} routes={routes}/>, document.getElementById('main'))
